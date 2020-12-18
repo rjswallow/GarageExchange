39750a1f0ea5118be175c153f13d93b3b113bd49
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-12-17 21:16:27 -0800

    tested a post route

--- Develop/schema.sql
+++ Develop/schema.sql
@@ -1,2 +1,41 @@
 DROP DATABASE IF EXISTS garageExchange;
 CREATE DATABASE garageExchange;
+
+INSERT INTO User (email, password)
+VALUES ("sam@sam.org", "helloworld"), ("Awa@awa.com", "hellohello"), ("Paul@paul.edu", "hithere"), ("Rober@robert.com", "hithereall");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Clearwater Hot Tub", "Quarantine in style ;) Hot tub hasn't been used for a while. Includes the whole setup, if desired: cover, heater, pumps. Needs to be dismantled and loaded on site. Complete with manuals and service records.", "December 12, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("6 cup Rice cooker", "Fully functional has a small steamer basket, measuring cup, rice paddle", "December 10, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Specialized Stumpjumper M2 Comp", "Specialized Stumpjumper M2 Comp Mountain Bike: 17 inch Frame. Original owner. Everything works great. Frame construction is TIG-welded. Frame Material is M2XMetal Matrix Ceramic Composite, butted. Front forks are Rock Shox Judy XC. Fork material is aluminum/magnesium, triple-clamp crown. Brakeset is Avid 1D-20 brakes with Avid SD-1.9 L levers. Shift levers are Shimano Deore XT RapidFire SL, Crankset is Specialized Son of Strong Arm, 22/32/42 teeth. Pedals are Ritchey Logic. Rear cogs are 8-speed, 11-30 teeth. Saddle is Specialized Prolong Revolution Carbotech with kevlar edge. Handlebars are Richey ForceLite. Handlebar stem is aluminum. Wheel size is 26 inch. Spoke brand is Ritchey Logic stainless steel with alloy nipples. This bike is in excellent condition. It has had very little use.", "December 16, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Unicycle new condition", "Unicycle new condition has a fresh paint job and a nice new tire. Rides very good and has had very little use.", "December 18, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Dinning table with 4 chairs", "Dinning table with four chairs.
+Dinning table is very durable and heavy(granite). Can be disassembled for hauling away.", "December 14, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Pearl Jam poster & beer cans", "Empty Pearl Jam beer cans and poster from the 2018 Home Shows 23 x 11 newspaper poster from 2018 Home shows", "December 16, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Daisy Duck t-shirt, coffee mug, Donald Duck ring", "Donald Duck adjustable ring (size 7 or smaller), new Daisy Duck t-shirt woman's size medium, and new Daisy Duck coffee / tea mug.", "December 16, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("IPHONE XS MAX 256 GOLD unlocked", "Unlocked Gold (rare color) Iphone XS Max 256g comes with charger and ready to be used out of the box, only minor chip on top otherwise in perfect condition.", "December 17, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("1972 Columbia sailboat", "Inside and out needs some cleaning up and basic restoration. It's a 1972 columbia 26 Has mount for outboard. Sails are in the boat I believe. Currently located at the Marina at Browns Point, however the boat would have to leave the marina once gifted.", "December 17, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Gorgeous tiles and bathroom sinks", "Beautiful Bizassa glass tiles. They can go indoors or out, vertically or horizontally and around corners. 19 boxes @ ~ 16 square feet per box, plus two matching bowl sinks.", "December 17, 2020");
+
+INSERT INTO Posts (item, description, date)
+VALUES ("Specialized Hardrock Sport Disc", "Selling my Specialized Hardrock Sport Disc mountain bike.
+RST Gila suspension forks, Acera front derailleur, XTR rear. Triple crank, 8-speed cassette, 26 inch wheels, Disc brakes", "December 17, 2020");
+
