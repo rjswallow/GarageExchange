e0447468f74bbf951a06f31c5d987e480e7db7a3
Awa <francisawa2@gmail.com>
2020-12-19 12:31:57 -0800

    saving changes

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -31,20 +31,51 @@ module.exports = function(app) {
     res.redirect("/");
   });
   
+  app.get("/", function(req, res) {
+    connection.query("SELECT * FROM garageExchnage;", function(err, data) {
+      if (err) throw err;
+  
+      // Test it
+      // console.log('The solution is: ', data);
+  
+      // Test it
+      // return res.send(data);
+  
+      res.render("index", { garageExchange: data });
+    });
+  });
+  
+  // Post route -> back to home
+  app.post("/", function(req, res) {
+    // Test it
+    // console.log('You sent, ' + req.body.task);
+  
+    // Test it
+    // return res.send('You sent, ' + req.body.task);
+  
+    // When using the MySQL package, we'd use ?s in place of any values to be inserted, which are then swapped out with corresponding elements in the array
+    // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
+    // https://en.wikipedia.org/wiki/SQL_injection
+    connection.query("INSERT INTO garageExchange VALUES (?)", [req.body.task], function(err, result) {
+      if (err) throw err;
+  
+      res.redirect("/");
+    });
+  });
 
-  // Route for getting some data about our user to be used client side
-  // app.get("/api/user_data", function(req, res) {
-  //   if (!req.user) {
-  //     // The user is not logged in, send back an empty object
-  //     res.json({});
-  //   } else {
-  //     // Otherwise send back the user's email and id
-  //     // Sending back a password, even a hashed password, isn't a good idea
-  //     res.json({
-  //       email: req.user.email
-  //       id: req.user.id
-  //     });
-  //   }
-  // });
+//   // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email
+  
+      });
+    }
+  });
 };
 
