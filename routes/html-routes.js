6d5a7fcf5f5085c2015dafcf568d3862d64b9081
Paul Besliu <besliu_marius@yahoo.com>
2020-12-21 18:53:47 -0800

    Update file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -96,29 +96,24 @@ module.exports = function(app) {
     res.render("index", {posts:allPosts});
   });
 
-  app.get("/signup", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
-  // app.get("/login", function(req, res) {
-  //   // If the user already has an account send them to the members page
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.render("login");
-  // });
 
   app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    // // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
+    res.render("login");
   });
+
+  app.get("/signup", function(req, res) {
+    // // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
+    res.render("signup");
+  });
+
+
 
   app.get("/post", function(req, res) {
     // // If the user already has an account send them to the members page
