50b451398cd0fa07ea39ea4baab18aa0c9f543e1
Sam Rodriguez <rodriguez_414@yahoo.com>
2020-12-22 17:25:05 -0800

    added heroku connection code to server.js

--- server.js
+++ server.js
@@ -42,3 +42,14 @@ db.sequelize.sync().then(function () {
     );
   });
 });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'influence',
+    database: 'garageExchange'
+  });
+};
